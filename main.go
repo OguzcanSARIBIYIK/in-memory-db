cda398299ddd2c04bfe75f2c769d2e0e502f0b78
OguzcanSARIBIYIK <oguzcan_746@hotmail.com>
2021-07-29 17:06:05 +0300

    Added >>> validation

--- main.go
+++ main.go
@@ -4,17 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"in-memory-db/indb"
-	"io/ioutil"
-	"log"
 	"net/http"
 )
 
 var DB *indb.InDB
-
-type Add struct {
-	Key   *string
-	Value *interface{}
-}
 
 type Response struct {
 	Code int
@@ -25,26 +18,27 @@ func main() {
 	DB = indb.Init()
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		var req Add
 
-		reqBody, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
+		key := r.URL.Query().Get("key")
+		if key == "" {
+			rw.WriteHeader(http.StatusBadRequest)
+			res, _ := json.Marshal(Response{Code: http.StatusBadRequest, Data: "Key is required."})
+			rw.Write(res)
+			return
 		}
-		json.Unmarshal(reqBody, &req)
+		value := r.URL.Query().Get("value")
+		if value == "" {
+			rw.WriteHeader(http.StatusBadRequest)
+			res, _ := json.Marshal(Response{Code: http.StatusBadRequest, Data: "Value is required."})
+			rw.Write(res)
+			return
+		}
 
 		rw.Header().Set("Content-Type", "application/json")
 
-		if req.Value != nil && req.Key != nil {
-			fmt.Println(*req.Value)
-			DB.Save(*req.Key, *req.Value)
-			res, _ := json.Marshal(Response{Code: http.StatusOK, Data: "Veri Kaydedildi."})
-			rw.Write(res)
-		} else {
-			rw.WriteHeader(http.StatusBadRequest)
-			res, _ := json.Marshal(Response{Code: http.StatusBadRequest, Data: "Veri Kaydedilemedi."})
-			rw.Write(res)
-		}
+		DB.Save(key, value)
+		res, _ := json.Marshal(Response{Code: http.StatusOK, Data: "Data saved."})
+		rw.Write(res)
 
 	})
 
@@ -63,17 +57,4 @@ func main() {
 		panic(err)
 	}
 
-	/* db.Save("Age", "23")
-	db.Save("Age", "31")
-	age, err := db.Get("Age")
-	fmt.Println(age, err)
-	//db.Delete("Age")
-	db.Save("Name", "Oğuzcan")
-	name, err := db.Get("Name")
-	fmt.Println(name, err)
-	allData := db.GetAll()
-
-	for k, v := range allData {
-		fmt.Printf("Key : %v - Value : %v \n", k, v)
-	} */
 }
